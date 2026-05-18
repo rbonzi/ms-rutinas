26734892b9c5f910c80fbfb959926feb7b2fbbd7
rbonzi <benjaminosorio902@gmail.com>
2026-05-18 00:16:36 -0400

    ms de rutinas terminado

---
+++ src/main/java/com/rutinas/rutinas/RutinasApplication.java
@@ -0,0 +1,13 @@
+package com.rutinas.rutinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutinasApplication.class, args);
+	}
+
+}
